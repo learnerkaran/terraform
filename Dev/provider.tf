fb8a2060e4d1e00da44d0ab4563e3240f16e112e
learnerkaran <168950203+learnerkaran@users.noreply.github.com>
2024-11-21 22:51:20 +0530

    asd

--- Dev/provider.tf
+++ Dev/provider.tf
@@ -1,6 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
   profile = "config"
+  shared_credentials_files = ["/root/.aws/credentials"]
     default_tags {
                 tags = {
                     name = "3tier"
@@ -15,6 +16,6 @@ terraform {
     dynamodb_table = "backend-db-backend-block"
     region = "ap-south-1"
         profile = "config"
-        shared_credentials_files = ["/home/anup/.aws/credentials"]
+        shared_credentials_files = ["/root/.aws/credentials"]
   }
 }
